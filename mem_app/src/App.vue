d7410743de476631c44f5529d96a8f2d9ceda442
alrenyang <alren.yk@gmail.com>
2023-02-11 14:29:59 +0900

    ver0.1 commit

--- mem_app/src/App.vue
+++ mem_app/src/App.vue
@@ -5,20 +5,37 @@
       color="primary"
       dark
     >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>고객 관리 프로그램</v-toolbar-title>
-    <v-spacer/>
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
+      <v-toolbar-title>고객 관리 프로그램</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-app-bar>
-    
+    <v-navigation-drawer v-model="drawer">
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer padless color="primary" dark absolute>
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Made by Alren Yang</strong>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
-};
+  name: 'App',
+  data: () => ({
+      drawer: false,
+      group: null,
+    }),
+  
+}
 </script>
